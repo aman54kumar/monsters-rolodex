7f7e143ac7d1db2176a423b7b1d3a2473ba72233
Aman <aman54kumar@gmail.com>
2019-08-03 01:39:18 +0200

    Updates

---
+++ precache-manifest.add390b535f42e83f5e97aaebb4862c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "308ea4c49ad73eac7f689a762a039811",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "537293349fa42ec87897",
+    "url": "/monsters-rolodex/static/css/main.ab818c52.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "537293349fa42ec87897",
+    "url": "/monsters-rolodex/static/js/main.055c0973.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
